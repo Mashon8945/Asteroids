e6271f332d8876747623ee56384084d5aea87f99
Le'Mashon <Leonardlemashon@gmail.com>
2025-02-04 00:19:00 +0300

    player color change

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
                     shot.kill()
                     asteroid.split()
             
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(30) / 1000
 
 if __name__ == "__main__":
     main()
