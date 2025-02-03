569d3c93a0387d69a11f6a3f2fe3de7b7760f109
Le'Mashon <Leonardlemashon@gmail.com>
2025-02-03 22:22:08 +0300

    Destruction of asteroids

--- main.py
+++ main.py
@@ -48,6 +48,11 @@ def main():
                 print("Game over!")
                 return
             
+            for shot in shots:
+                if shot.check_collision(asteroid):
+                    shot.kill()
+                    asteroid.kill()
+            
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
